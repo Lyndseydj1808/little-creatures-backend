3ef92b580c3c92bb722cded1b0cd20414b9af075
Lyndseydj1808 <lyndseydj1808@gmail.com>
2026-08-16 14:05:20 -0500

    initial commit

---
+++ src/main/java/com/lyndsey/littlecreatures/little_creatures_backend/LittleCreaturesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lyndsey.littlecreatures.little_creatures_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleCreaturesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleCreaturesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lyndsey/littlecreatures/little_creatures_backend/LittleCreaturesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyndsey.littlecreatures.little_creatures_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleCreaturesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
